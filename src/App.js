e090204ce38e38fd48200688bea1eeaf19887a62
silly <bxvipsky@gmail.com>
2018-09-22 18:22:33 +0800

    Ant Design of React 引入

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'antd';
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 class App extends Component {
   render() {
     return (
@@ -13,6 +14,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <Button type="primary" shape="circle" icon="search" />
+          <Button type="primary" icon="search">Search</Button>
+          <Button shape="circle" icon="search" />
+          <Button icon="search">Search</Button>
+          <br />
+          <Button shape="circle" icon="search" />
+          <Button icon="search">Search</Button>
+          <Button type="dashed" shape="circle" icon="search" />
+          <Button type="dashed" icon="search">Search</Button>
       </div>
     );
   }
